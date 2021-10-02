9b37968527360a4b3a8a3a26efbcdedcb2f5e87a
Taeuk Gwak <gwak2837@kakao.com>
2021-10-02 16:43:46 +0900

    Update options.ts

--- src/utils/options.ts
+++ src/utils/options.ts
@@ -20,14 +20,16 @@ export const upbitWebSocketRequestOption = [
 
 export const TICK_INTERVAL = 60
 
-export const MFI_PERIOD = 14
-export const MFI_BUYING_SIGNAL = 20
-
 export const RSI_PERIOD = 14
 export const RSI_BUYING_SIGNAL = 25
+export const RSI_MUST_BUYING_SIGNAL = 10
 
 export const CCI_PERIOD = 14
 export const CCI_BUYING_SIGNAL = -150
+
+export const MFI_PERIOD = 14
+export const MFI_BUYING_SIGNAL = 20
+export const MFI_MUST_BUYING_SIGNAL = 5
 
 export const WILLIAMSR_PERIOD = 14
 export const WILLIAMSR_BUYING_SIGNAL = -20
@@ -43,8 +45,12 @@ export type Indicator = {
 
 export function goodToBuy(prevIndicator: Indicator, newIndicator: Indicator) {
   return (
-    newIndicator.rsi < 20 ||
-    (newIndicator.rsi < RSI_BUYING_SIGNAL && newIndicator.cci < CCI_BUYING_SIGNAL) ||
+    newIndicator.rsi < RSI_MUST_BUYING_SIGNAL ||
+    newIndicator.rsi < MFI_MUST_BUYING_SIGNAL ||
+    (prevIndicator.rsi < RSI_BUYING_SIGNAL &&
+      newIndicator.rsi > RSI_BUYING_SIGNAL &&
+      prevIndicator.cci < CCI_BUYING_SIGNAL &&
+      newIndicator.cci > CCI_BUYING_SIGNAL) ||
     (newIndicator.cci < CCI_BUYING_SIGNAL &&
       newIndicator.mfi < MFI_BUYING_SIGNAL &&
       newIndicator.willliamsR < WILLIAMSR_BUYING_SIGNAL)
