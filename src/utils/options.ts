710ff25468d896cdbf96dac65a67a272ef380061
Taeuk Gwak <gwak2837@kakao.com>
2021-10-02 11:51:26 +0900

    Add buying condition

--- src/utils/options.ts
+++ src/utils/options.ts
@@ -1,10 +1,12 @@
+import { v4 } from 'uuid'
+
 export const ACCESS_KEY = process.env.UPBIT_OPEN_API_ACCESS_KEY ?? ''
 export const SECRET_KEY = process.env.UPBIT_OPEN_API_SECRET_KEY ?? ''
 export const UPBIT_API_URL = 'https://api.upbit.com'
 
 export const upbitWebSocketRequestOption = [
   {
-    ticket: process.env.TICKET,
+    ticket: v4(),
   },
   {
     type: 'ticker',
@@ -28,6 +30,23 @@ export const CCI_PERIOD = 14
 export const CCI_BUYING_SIGNAL = -150
 
 export const WILLIAMSR_PERIOD = 14
-export const WILLIAMSR_BUYING_SIGNAL = 14
+export const WILLIAMSR_BUYING_SIGNAL = -20
 
-export const AUTO_SELLING_RATIO = 1.002
+export const AUTO_SELLING_RATIO = 1.0015
+
+export type Indicator = {
+  rsi: number
+  mfi: number
+  cci: number
+  willliamsR: number
+}
+
+export function goodToBuy(prevIndicator: Indicator, newIndicator: Indicator) {
+  return (
+    newIndicator.rsi < 20 ||
+    (newIndicator.rsi < RSI_BUYING_SIGNAL && newIndicator.cci < CCI_BUYING_SIGNAL) ||
+    (newIndicator.cci < CCI_BUYING_SIGNAL &&
+      newIndicator.mfi < MFI_BUYING_SIGNAL &&
+      newIndicator.willliamsR < WILLIAMSR_BUYING_SIGNAL)
+  )
+}
