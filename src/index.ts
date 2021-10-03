7c636db4c7c93ce407b5e083ca2669a2739624ab
Taeuk Gwak <gwak2837@kakao.com>
2021-10-03 16:25:23 +0900

    Update auto buying and selling

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import {
 } from './utils/options'
 import { logWriter, tickWriter } from './utils/writer'
 
-let tickIth = 1
+let tickIth = 0
 const tempTicks = new Array()
 const tempVolumes = new Array()
 
@@ -21,8 +21,6 @@ let prevIndicator = {
   cci: 0,
   willliamsR: -50,
 }
-
-let buy = true
 
 const ws = new WebSocket('wss://api.upbit.com/websocket/v1')
 
@@ -40,6 +38,8 @@ ws.on('message', async (data) => {
 
   tempTicks.push(tick.tp)
   tempVolumes.push(tick.tv)
+
+  tickIth = tickIth++ < TICK_INTERVAL ? tickIth : 1
 
   if (tickIth === TICK_INTERVAL) {
     const newTick = {
@@ -59,7 +59,11 @@ ws.on('message', async (data) => {
 
     const buyingCondition = goodToBuy(prevIndicator, newIndicator)
 
-    if (buyingCondition && buy) {
+    prevIndicator = newIndicator
+    tempTicks.length = 0
+    tempVolumes.length = 0
+
+    if (buyingCondition) {
       const buyingOrderResult = await orderCoin({
         market: tick.cd,
         ord_type: 'price', // limit로 개선 필요
@@ -69,67 +73,50 @@ ws.on('message', async (data) => {
         // volume: '0.0002',
       })
 
-      logWriter.write(`${printNow()} bid ${JSON.stringify(buyingOrderResult)}\n`)
+      if (buyingOrderResult.error) {
+        logWriter.write(`${printNow()} buy error ${JSON.stringify(buyingOrderResult)}\n`)
+      } else {
+        const buyingOrderDetail = await waitUntilOrderDone(buyingOrderResult.uuid)
 
-      if (!buyingOrderResult.error) {
-        const { trades } = await waitUntilOrderDone(buyingOrderResult.uuid)
+        const averageBuyingPrice =
+          buyingOrderDetail.trades.reduce((acc, current) => acc + +current.funds, 0) /
+          +buyingOrderDetail.executed_volume
 
-        trades.forEach(async (trade) => {
-          const sellingOrderResult = await orderCoin({
-            market: trade.market,
-            ord_type: 'limit',
-            price: `${ceilUpbitPrice(+trade.price * AUTO_SELLING_RATIO)}`,
-            side: 'ask',
-            volume: trade.volume,
-          })
+        logWriter.write(`${printNow()} buy detail ${JSON.stringify(buyingOrderDetail)}\n`)
 
-          logWriter.write(`${printNow()} ask ${JSON.stringify(sellingOrderResult)}\n`)
-        })
+        const sellingOption = {
+          market: buyingOrderDetail.market,
+          ord_type: 'limit' as const,
+          price: `${ceilUpbitPrice(averageBuyingPrice * AUTO_SELLING_RATIO)}`,
+          side: 'ask' as const,
+          volume: buyingOrderDetail.executed_volume,
+        }
+
+        while (true) {
+          const sellingOrderResult = await orderCoin(sellingOption)
+
+          logWriter.write(`${printNow()} sell result ${JSON.stringify(sellingOrderResult)}\n`)
+
+          if (sellingOrderResult.error) {
+            logWriter.write(`${printNow()} sell error ${JSON.stringify(sellingOption)}\n`)
+          } else {
+            break
+          }
+        }
       }
     }
-
-    tempTicks.length = 0
-    tempVolumes.length = 0
-    prevIndicator = newIndicator
 
     const tickLog = [printNow(), ...Object.values(newTick), ...Object.values(newIndicator)]
     if (buyingCondition) tickLog.push('O')
     tickWriter.write(tickLog.join(',') + '\n')
   }
-
-  tickIth = ++tickIth <= TICK_INTERVAL ? tickIth : 1
 })
 
 async function waitUntilOrderDone(uuid: string) {
   while (true) {
     const buyingOrder = await getOrder(uuid)
+    const state = buyingOrder.state
 
-    if (buyingOrder.state === 'done' || buyingOrder.state === 'cancel') return buyingOrder
+    if (state === 'done' || state === 'cancel') return buyingOrder
   }
 }
-
-// async function buyAndSell() {
-//   const marketOrderBody = {
-//     market: 'KRW-BTC',
-//     ord_type: 'price',
-//     side: 'bid',
-//     price: '6000',
-//   }
-
-//   const body = await order(marketOrderBody)
-
-//   const { trades } = await getOrder(body.uuid)
-
-//   // order 미체결 상태인지 확인하기
-
-//   trades.forEach(async (trade) => {
-//     const response = await order({
-//       market: trade.market,
-//       ord_type: 'limit',
-//       side: 'ask',
-//       price: `${ceilUpbitPrice(+trade.price * 1.002)}`,
-//       volume: trade.volume,
-//     })
-//     logWriter.write(JSON.stringify(response))
-//   })
-// }
